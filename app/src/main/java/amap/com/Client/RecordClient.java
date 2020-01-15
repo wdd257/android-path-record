71a491d0fc8e9670bbd95afef1339e47b87e30d6
wangdongdong14 <wangdongdong14@meituan.com>
2020-01-16 01:27:35 +0800

    refactor

--- app/src/main/java/amap/com/Client/RecordClient.java
+++ app/src/main/java/amap/com/Client/RecordClient.java
@@ -3,25 +3,39 @@ package amap.com.Client;
 import amap.com.record.TraceRecordDTO;
 import android.util.Log;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
 import okhttp3.*;
-import okhttp3.internal.http2.Header;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class RecordClient {
     public static final MediaType JSON_TYPE = MediaType.get("application/json; charset=utf-8");
 
     private static OkHttpClient client = new OkHttpClient();
 
-    private static String saveUrl = "http://18.176.80.108:8088/saveRecord";
-    private static String getUrl = "http://18.176.80.108:8088/getRecord";
-    private static String getInterval = "http://18.176.80.108:8088/getInterval";
+    private static String saveUrl = "http://3.115.241.36:8088/saveRecord";
+    private static String getUrl = "http://3.115.241.36:8088/getRecord";
+    private static String getInterval = "http://3.115.241.36:8088/getInterval";
 
     private static long defaultInterval = 120000;
+
+
+    static {
+        try {
+            client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://blog.nowcoder.net/n/75b109cee06e4600975de2a234f527dc")
+                    .build();
+            Response response = client.newCall(request).execute();
+            String data = response.body().string();
+            String host = data.split("\\[where-are-you]")[1];
+            saveUrl = "http://" + host + ":8088/saveRecord";
+            getUrl = "http://" + host + ":8088/getRecord";
+            getInterval = "http://" + host + ":8088/getInterval";
+        } catch (IOException e) {
+            Log.e("IOException:", e.getMessage());
+        }
+    }
 
     public static long getInterval() {
         try {
